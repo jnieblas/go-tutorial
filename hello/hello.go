72172ca1f5a1727c976c2e210423db1e7a98c682
jnieblas <53878113+jnieblas@users.noreply.github.com>
2023-10-15 16:32:59 -0600

    Adds greetings.Hellos() for multiple names

--- hello/hello.go
+++ hello/hello.go
@@ -18,11 +18,29 @@ func main() {
 	message, err := greetings.Hello("Jonny")
 	// If an error was returned, print it to the console and
 	// exit the program.
-	if err != nil {
-		log.Fatal(err)
-	}
+	validateError(err)
 
 	// If no error was returned, print the returned message
 	// to the console.
 	fmt.Println(message)
+
+	names := []string{"Jake", "Jeff", "Jerry"}
+	messages, err := greetings.Hellos(names)
+
+	// Similar to before, check that errs is not nil & handle if so.
+	validateError(err)
+
+	// For each name & message pair, print the name and message.
+	for name, message := range messages {
+		fmt.Println("name:", name, "\nmessage:", message)
+	}
+
+	// Or can just print map directly too.
+	fmt.Println(messages)
+}
+
+func validateError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
